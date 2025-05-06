ab326aafa8c6b2e1e314c9972d40ec5e5697ae62
Naufal Azzmi <naufal.chroome@gmail.com>
2025-05-06 14:20:03 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/form-builder/",
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
